9483c03385067ac05c4a3b3e4f703c8b122efda7
MariaAngelicaRodriguezCoto <mrodriguezco@ucenfotec.ac.cr>
2025-07-04 10:58:11 -0600

    Changes

--- .idea/src/Data/ApplicationDbContext.cs
+++ .idea/src/Data/ApplicationDbContext.cs
@@ -1,7 +1,8 @@
 namespace DefaultNamespace;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
@@ -9,4 +10,17 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Book> Books { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        builder.Entity<Book>(entity =>
+        {
+            entity.Property(e => e.Title).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.Author).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.Genre).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.Description).HasMaxLength(500);
+        });
+    }
 }
